834cfbcb3a0a6c0e06c236c0f855a8228da7046a
Benjamin Szabo <benny03.szabo@gmail.com>
2026-02-10 17:25:56 -0600

    finished debug

--- cfg_parsing/cfgparsing.cpp
+++ cfg_parsing/cfgparsing.cpp
@@ -1,5 +1,5 @@
 //code
-#include <vector>
+#include <set>
 #include <iomanip>
 #include <string>
 #include <cstdint>
@@ -9,24 +9,19 @@
 using namespace std;
 
 void cyk(const char* s, int n, int start_state, const multimap<unsigned int, unsigned int> & inv_cfg){
-    vector<int> table[n][n];
+    set<int> table[n][n];
 
-    //go through each row
     for(int i = 0; i < n; ++i){
-        //each column in a row (upper left triangle)
         for(int j = 0; j < n - i; ++j){
             if (i == 0) {
                 auto res = inv_cfg.equal_range(static_cast<uint16_t>(s[j])); 
-                if (res.first == res.second) {table[i][j].push_back(-1);}
-                else{
-                    for (auto it = res.first; it != res.second; ++it) {
-                        table[i][j].push_back(it->second);
-                    }
+                for (auto it = res.first; it != res.second; ++it) {
+                    table[i][j].insert(it->second);
                 }
             } else {
                 //check all substrings
-                for(int k = 1, l = i - 1; k < (i - 1); --l, ++k){
-                    if(table[i-k][j].front() == -1 || table[i-l][j+l].front() == -1){ table[i][j].push_back(-1); }
+                for(int k = 1, l = i; k <= i ; --l, ++k){
+                    if(table[i-k][j].empty() || table[i-l][j+l].empty()){ continue; }
                     else{
                         for(int val1 : table[i-k][j]){
                             for(int val2 : table[i-l][j+l]){
@@ -34,7 +29,7 @@ void cyk(const char* s, int n, int start_state, const multimap<unsigned int, uns
                                         static_cast<uint16_t>(val1) << 8 | static_cast<uint16_t>(val2)
                                 );
                                 for (auto it = res.first; it != res.second; ++it) {
-                                    table[i][j].push_back(it->second);
+                                    table[i][j].insert(it->second);
                                 }
                             }
                         }
@@ -44,11 +39,8 @@ void cyk(const char* s, int n, int start_state, const multimap<unsigned int, uns
         }
     }
 
-    if (table[n-1][n-1].front() == start_state) {
-        cout << "yes" << endl;
-    } else {
-        cout << "no" << endl;
-    }
+
+    cout << (table[n-1][0].count(start_state) ? "yes" : "no") << endl;
 }
 
 int main() {
